62cf57c0573392fa3be1222e30c1af76e61e02bf
BrianKendalBucek <91170533+BrianKendalBucek@users.noreply.github.com>
2023-06-06 21:04:00 -0700

    example of manual transformation of array into instance of a specific constructor function

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,15 @@
 import { Product } from "./product.model";
 
-const p1 = new Product('A Book', 12.99);
+const products = [
+  { title: "A Carpet", price: 29.99 },
+  { title: "A Book", price: 10.99 },
+];
 
-console.log(p1.getInformation());
+// const p1 = new Product("A Book", 12.99);
+const loadedProducts = products.map(prod => {
+  return new Product(prod.title, prod.price);
+});
+
+for (const prod of loadedProducts) {
+  console.log(prod.getInformation());
+}
